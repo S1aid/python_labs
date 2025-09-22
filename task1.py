0837785e7c20fe16b3c825a11a4a850d696e61b0
S1aid <serghantrm@gmail.com>
2025-09-22 19:44:07 +0300

    task

--- task1.py
+++
@@ -1,7 +0,0 @@
-def print_initials():
-    sername = input("Введите вашу фамилию: ")
-    name = input("Введите ваше имя: ")
-    dad_name = input("Введите ваше отчество: ")
-    print(f'{sername} {name[0]}. {dad_name[0]}.')
-
-print_initials()
--- task8.py
+++
@@ -1,8 +0,0 @@
-def is_polindrom():
-    text = input("Введите слово: ")
-    if text.lower() == text.lower()[::-1]:
-        print("True")
-    else:
-        print("False")
-
-is_polindrom()
